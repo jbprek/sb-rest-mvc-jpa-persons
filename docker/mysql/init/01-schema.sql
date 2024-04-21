d14c41da4d3807ff8820191e72532b3d3ff30357
jbprek <jbprek@gmail.com>
2024-04-21 11:46:25 +0100

    WIP

--- docker/mysql/init/01-schema.sql
+++ docker/mysql/init/01-schema.sql
@@ -1,12 +1,15 @@
 create database persons;
 use persons;
 
-create table persons (
-                         person_id     bigint        auto_increment primary key,
-                         first_name    varchar(255)  not null,
-                         last_name     varchar(255)  not null,
-                         birth_date    date          not null,
-                         country       varchar(3)    not null );
+create table persons
+(
+    person_id  bigint auto_increment primary key,
+    first_name varchar(255) not null,
+    last_name  varchar(255) not null,
+    birth_date date         not null,
+    country    varchar(3)   not null,
+    version    bigint
+);
 
 CREATE USER 'persons'@'%' IDENTIFIED BY 'persons';
 GRANT ALL PRIVILEGES ON persons.* TO 'persons'@'%';
