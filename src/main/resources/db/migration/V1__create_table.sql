2947994018c0b89093060aa4ebb6813584e6bd22
jbprek <jbprek@gmail.com>
2024-05-12 07:51:39 +0100

    Flyway maven setup

---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,15 @@
+CREATE SEQUENCE person_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE persons
+(
+    person_id  NUMBER(19, 0) NOT NULL,
+    first_name VARCHAR2(255),
+    last_name  VARCHAR2(255),
+    birth_date date,
+    country    VARCHAR2(255),
+    version    NUMBER(38, 0),
+    CONSTRAINT pk_persons PRIMARY KEY (person_id)
+);
+
+ALTER TABLE persons
+    ADD CONSTRAINT uniqueNameBodCountry UNIQUE (first_name, last_name, birth_date, country);
---
+++ src/main/resources/db/migration/V2__initial_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'James', 'Smith', TO_DATE('1980-03-01', 'YYYY-MM-DD'), 'USA');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'John', 'Johnson', TO_DATE('1982-04-02', 'YYYY-MM-DD'), 'CAN');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Robert', 'Williams', TO_DATE('1975-05-03', 'YYYY-MM-DD'), 'GBR');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Michael', 'Brown', TO_DATE('1990-06-04', 'YYYY-MM-DD'), 'AUS');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'William', 'Jones', TO_DATE('1988-07-05', 'YYYY-MM-DD'), 'USA');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Alicia', 'Fernandez', TO_DATE('1993-09-28', 'YYYY-MM-DD'), 'ESP');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Yuki', 'Tanaka', TO_DATE('1991-10-29', 'YYYY-MM-DD'), 'JPN');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Chen', 'Wang', TO_DATE('1984-11-30', 'YYYY-MM-DD'), 'CHN');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Maria', 'Garcia', TO_DATE('1979-12-31', 'YYYY-MM-DD'), 'MEX');
+INSERT INTO persons (person_id, first_name, last_name, birth_date, country)
+VALUES (person_seq.nextval, 'Olivia', 'Martinez', TO_DATE('1985-01-01', 'YYYY-MM-DD'), 'USA');
